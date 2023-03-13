a99161a4a4736b5e92de500420ec873ad6cf7f1d
arafatyamin <mdh631987@gmail.com>
2023-03-14 00:42:29 +0600

    add chat app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const socket = io.connect('http://localhost:5000')
+  const [message, setMessage] = useState('');
+  const [getMessage, setGetMessage] = useState('');   
+  const [room, setRoom] = useState('');   
+
+  const handleSend = () =>{
+    socket.emit("chatEvent", {message,room});
+  };
+
+  useEffect(()=>{
+    socket.on("showChat", (data)=>{
+      console.log(data)
+      setGetMessage(data.message);
+    });
+  },[socket]);
+
+  const handleRoom = () =>{
+    socket.emit("joinRoom", room);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full">
+      <h1>hello react and socket.io-client</h1>
+      <div>
+        <h1 className="text-3xl text-green-700 text-left">{message}</h1>
+        <h1 className="text-3xl text-blue-700 text-right mr-4">{getMessage}</h1>
+      </div>
+
+      <div className="flex flex-col mx-auto justify-center items-center">
+      <div>
+      <input 
+      onBlur={(e) => setRoom(e.target.value)} 
+      type="text"
+      placeholder="rome..." 
+      className='py-2 border border-gray-200' /> 
+      <button onClick={handleRoom}
+       className="p-2 
+       bg-gray-300">join room</button> 
+      </div>
+
+      <input onBlur={(e) => setMessage(e.target.value)} 
+      type="text"
+      placeholder="message..." 
+      className='py-2 border border-gray-200' />
+      <button onClick={handleSend}
+       className="p-2 
+       bg-gray-300">send</button>
+      </div>
     </div>
   );
 }
---
+++ src/Pages/Home/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <h1 className="text-2xl w-full text-center">Home</h1>
+         this is a navigation   
+        </div>
+    );
+};
+
+export default Nav;
